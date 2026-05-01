c4ef0e022d095f58616a6d50acfecafab9efc5aa
Justmore5mins <justmore5mins@gmail.com>
2026-05-01 23:30:21 +0800

    Started woring on the steering motor, detailed in PR

--- Control.cpp
+++ Control.cpp
@@ -1,7 +1,8 @@
+#include <avr/interrupt.h>
+#include "Servo.h"
 #include "AS5600.h"
 #include "Arduino.h"
 #include "Constants.h"
-#include "Wire.h"
 #include "Control.h"
 
 
@@ -15,39 +16,60 @@ SoftwarePID::SoftwarePID(ClosedLoopConfig pid){
   this->setpoint = 0;
 };
 
-float SoftwarePID::calculateOutput(float current, float setpoint, long dT){
-  float delta = current - setpoint;
+void SoftwarePID::withSetpoint(float setpoint){
+  this->setpoint = setpoint;
+}
+
+float SoftwarePID::calculateOutput(float current, float setpoint, long dT, boolean isStandardized){
+  float delta = (current - setpoint)/(isStandardized ? setpoint : 1);
   return 
     pid.kP *   delta +
     pid.kI * ( delta * dT) + 
     pid.kD * ( delta / dT);
 }
 
-float SoftwarePID::calculateOutput(float current, long dT){
-  return calculateOutput(current, setpoint , dT);
+float SoftwarePID::calculateOutput(float current, long dT, boolean isStandardized){
+  return calculateOutput(current, setpoint , dT, isStandardized);
+}
 }
 
-Drivetrain::Drivetrain(SoftwarePID LeftPID, SoftwarePID RightPID, FeedForwardConfig WheelFF) :
+SG90::SG90(int ID, SoftwarePID ctrl):
+  control(ctrl){
+    motor = Servo();
+    motor.attach(3);
+}
+
+float SG90::getPosition(){
+  return 0; //TODO: Try out the way to get the position measure.
+}
+
+void SG90::set(float percent){
+  motor.write(map(percent, 0, 100, 0,100)); //TODO: Try out the range
+}
+
+void SG90::turnTo(float degree){
+  control.withSetpoint(degree);
+  while (!((degree - getPosition())/degree < 0.05)) {
+    set(control.calculateOutput(getPosition(), 15, true));
+    delay(15);
+  }
+}
+
+Drivetrain::Drivetrain(SoftwarePID LeftPID, SoftwarePID RightPID, SoftwarePID SteerPID, FeedForwardConfig WheelFF) :
   LeftPID(LeftPID),
   RightPID(RightPID),
+  SteerPID(SteerPID),
   ff(WheelFF),
   LeftEncoder(AS5600()),
   RightEncoder(AS5600()){
   };
-  
-MotorState Drivetrain::getLeftState(){
-  MotorState currentState = MotorState{
-   (long int)LeftEncoder.getCumulativePosition(),
-    LeftEncoder.getAngularSpeed(AS5600_MODE_RPS, true),
-    millis()
-  };
-}
 
-MotorState Drivetrain::getRightState(){
-  MotorState currentState = MotorState{
-   (long int)RightEncoder.getCumulativePosition(),
-    RightEncoder.getAngularSpeed(AS5600_MODE_RPS, true),
-    millis()
-  };
+/**
+Accepting postition in `Degrees`
+*/
+void Drivetrain::steer(float Position){
+  SteerPID.withSetpoint(Position);
+  while(!(((Position - getSteerState().CurrentPosition)/Position) < 0.05)){
+    
+  }
 }
-
